7af307c3b7da0d435e8debf786e117a745ed2c41
Onur Yukselen <onuryukselen@gmail.com>
2022-01-18 15:02:53 -0500

    minor bug

--- mapreads.py
+++ mapreads.py
@@ -47,7 +47,7 @@ def wrapbowtie2(bowtiedb, unpaired, outfile, scriptdir, trnafile, maxmaps = MAXM
     localmode = " "
     if local:
         localmode = " --local "
-    bowtiecommand = program+localmode+' -x '+bowtiedb+' -k '+str(maxmaps)+' --very-sensitive --ignore-quals --np 5 --reorder -p '+str(numcores)+' --un '+outfile+' .unmapped.fastq '+' -U '+unpaired
+    bowtiecommand = program+localmode+' -x '+bowtiedb+' -k '+str(maxmaps)+' --very-sensitive --ignore-quals --np 5 --reorder -p '+str(numcores)+' --un '+outfile+'.unmapped.fastq '+' -U '+unpaired
 
     #print >>sys.stderr, bowtiecommand
     temploc = os.path.basename(outfile)
